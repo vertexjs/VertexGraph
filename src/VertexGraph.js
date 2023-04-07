cbe504ec7f0d0b2726d4f37ee0d44e4b2a14ea66
nichodim <nickseagal321@gmail.com>
2023-04-07 15:37:36 -0400

    Removed minimized graph hardcoded styling

--- src/VertexGraph.js
+++ src/VertexGraph.js
@@ -572,10 +572,7 @@ function runGraph() {
         .style('height', VertexGraph.Config.svgHeight); 
     
     if (!VertexGraph.Config.fullScreenMode) {
-        web.style('left', '50%')
-        .style('transform', 'translateX(-50%)')
-        .style('margin-top', '5%')
-        .style('border', '2px solid black'); 
+        web.style('border', '2px solid black'); 
     } else {
         d3.select('#body').style('margin', 0)
             .style('overflow-y', 'hidden'); 
